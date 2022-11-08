f4b38a2d8a080ad6f90211d961798346f77e4f63
bashar4980 <sagor.ahmed3669@gmail.com>
2022-11-08 17:21:07 +0600

    Heder section is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Footer from './Shared/Footer';
 import Header from './Shared/Header';
 
 function App() {
   return (
    <>
    <Header></Header>
+   <Footer/>
    </>
   );
 }
--- src/Shared/Header.jsx
+++ src/Shared/Header.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
+import {Navbar, Button} from "flowbite-react";
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <Navbar  className=' border-b border-gray-200 dark:border-gray-600'
+  fluid={false}
+  rounded={true}
+>
+  <Navbar.Brand href="https://flowbite.com/">
+    <img
+      src="https://flowbite.com/docs/images/logo.svg"
+      className="mr-3 h-6 sm:h-9"
+      alt="Flowbite Logo"
+    />
+    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+      Flowbite
+    </span>
+  </Navbar.Brand>
+  <div className="flex md:order-2">
+    <Button>
+      Get started
+    </Button>
+    <Navbar.Toggle />
+  </div>
+  <Navbar.Collapse >
+    <Navbar.Link
+      href="/navbars"
+      active={true}
+    >
+      Home
+    </Navbar.Link>
+    <Navbar.Link href="/navbars">
+      About
+    </Navbar.Link>
+    <Navbar.Link href="/navbars">
+      Services
+    </Navbar.Link>
+    <Navbar.Link href="/navbars">
+      Pricing
+    </Navbar.Link>
+    <Navbar.Link href="/navbars">
+      Contact
+    </Navbar.Link>
+  </Navbar.Collapse>
+</Navbar>
     );
 };
 
